a955310bd08f5528455c9624625f7da9d002fb9c
khudabakshk <khudabakshkhan625@gmail.com>
2025-01-17 20:25:14 +0500

    """"

--- src/lib/action/user.js
+++ src/lib/action/user.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model"
-import connect from "../mongoose/connectDB"
+import {connect} from "../mongoose/connectDB"
 
 export const createOrUpdateUSer = async(
     id,
