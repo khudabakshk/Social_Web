ece0c5300a3e86ac1933ad324ce412dce7e72710
khudabakshk <khudabakshkhan625@gmail.com>
2025-01-21 21:52:56 +0500

    ""K""

--- src/app/(other)/page.js
+++ src/app/(other)/page.js
@@ -1,6 +1,8 @@
+"use client"
 import React from 'react'
-
+import { connectDB } from '../../lib/mongoose/connectDB'
 export default function page() {
+  connectDB()
   return (
     <div className='text-blue-500'>Home</div>
 
