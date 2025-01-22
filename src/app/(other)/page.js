1e1c335c2800bac19b4843793c96f0f3f7f3a548
khudabakshk <khudabakshkhan625@gmail.com>
2025-01-22 20:28:52 +0500

    ""k"

--- src/app/(other)/page.js
+++ src/app/(other)/page.js
@@ -4,13 +4,8 @@ import React from 'react'
 export default function page() {
 
   return (
-    <div className='min-h-screen max-w-xl mx-auto border-r border-l '>
-    <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200 '>
-      <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
- 
-    </div>
-  </div>
-
+    <div className='text-blue-500'>Home</div>
+    
 
   )
 }
