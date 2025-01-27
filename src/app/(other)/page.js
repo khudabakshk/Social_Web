c27a5fa03f590dceb860f2c9136c7e402bcd859a
khudabakshk <khudabakshkhan625@gmail.com>
2025-01-27 21:42:06 +0500

    "d"

--- src/app/(other)/page.js
+++ src/app/(other)/page.js
@@ -1,9 +1,13 @@
-
+import Input from '../../components/Input'
 import React from 'react'
 
 export default function page() {
-
   return (
-    <div className='text-blue-500'>Home</div>
+    <div className='min-h-screen max-w-xl mx-auto border-r border-l '>
+      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200 '>
+        <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
+    <Input/>
+      </div>
+    </div>
   )
 }
