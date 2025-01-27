70d77d4e0726c094bf67b7f8e3b20d3fb8323080
khudabakshk <khudabakshkhan625@gmail.com>
2025-01-27 21:31:18 +0500

    "K"

--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,8 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return <div className="flex justify-center items-center p-3">
+     <SignUp />
+  </div>
+ 
 }
