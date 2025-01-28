d083a6941d8551d430e3919dbb3b7a3c18803ed6
khudabakshk <khudabakshkhan625@gmail.com>
2025-01-28 21:09:22 +0500

    "G"

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,31 @@
+"use client"
 import React from 'react'
+import { FaImage } from "react-icons/fa";
+import { useUser } from '@clerk/nextjs';
+import { useRef } from 'react';
 
 export default function Input() {
+ const { user ,isSignedIn,isLoaded} = useUser();
+ const IMAGEFILEREF = useRef(null)
+if(!user || !isSignedIn || !isLoaded){
+    return null
+}
+const handleUploadImage = () =>{
+
+}
   return (
-    <div>
-      Input
+    <div className='flex  '> 
+        <img src={user.imageUrl} alt='user_img' />
+      <textarea noOfLines={2}/>
+      
+      <div className="">
+      <FaImage onClick={()=>{
+        IMAGEFILEREF.current.click()
+      }}  /> 
+      <input hidden  type='file' accept='images/*' ref={IMAGEFILEREF} onChange={handleUploadImage} />
+
+      <button>Post</button>
+      </div>
     </div>
   )
 }
