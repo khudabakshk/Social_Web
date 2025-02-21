d198a2eab0c32e80399b77ceb7984ab03f96d48a
khudabakshk <khudabakshkhan625@gmail.com>
2025-02-21 20:35:35 +0500

    """fhdfhd""

--- src/lib/models/post.model.js
+++ src/lib/models/post.model.js
@@ -11,7 +11,7 @@ const POSTSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+       
     },
     name: {
         type: String,
